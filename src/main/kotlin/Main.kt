ed67037a604d7fb7011bb50e163355c338d303ef
ayoog <20098135@mail.wit.ie>
2023-02-08 14:53:22 +0000

    Implement ScannerInput utility for robust console Io

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,29 @@
+import utils.ScannerInput
 import java.lang.System.exit
-import java.util.*
 
-val scanner = Scanner(System.`in`)
+
+
 
 fun main(args: Array<String>){
     runMenu()
 }
 
 fun mainMenu() : Int {
-    println("")
-    println("--------------------")
-    println("NOTE KEEPER APP")
-    println("--------------------")
-    println("NOTE MENU")
-    println("  1) Add a note")
-    println("  2) List all notes")
-    println("  3) Update a note")
-    println("  4) Delete a note")
-    println("-------------------")
-    println("  0 Exit")
-    println("-------------------")
-    println("==>> ")
-    return scanner.nextInt()
+    return ScannerInput.readNextInt(""" 
+         > ----------------------------------
+         > |        NOTE KEEPER APP         |
+         > ----------------------------------
+         > | NOTE MENU                      |
+         > |   1) Add a note                |
+         > |   2) List all notes            |
+         > |   3) Update a note             |
+         > |   4) Delete a note             |
+         > ----------------------------------
+         > |   0) Exit                      |
+         > ----------------------------------
+         > ==>> """.trimMargin(">"))
 }
+
 
 fun runMenu() {
     do {
@@ -33,7 +34,7 @@ fun runMenu() {
             3 -> updateNote()
             4 -> deleteNote()
             0 -> exitApp()
-            else -> println("Invalid option entered:" + option)
+            else -> println("Invalid option entered:  ${option}")
         }
     } while (true)
 }
